96d10670ea6dc85d4c0968d906760c89c83fab58
Niniko <ndautashvili@credo.ge>
2025-12-27 23:07:24 +0400

    change code for artifact running

--- Bolt.API/Program.cs
+++ Bolt.API/Program.cs
@@ -2,13 +2,23 @@ using Bolt.Application;
 using Bolt.Persistence;
 using Bolt.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Bolt API",
+        Version = "v1",
+        Description = "Bolt DDD Application API"
+    }
+);
+});
 
 // Add Application layer
 builder.Services.AddApplication();
@@ -29,11 +39,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
     // Apply migrations automatically in development
     using var scope = app.Services.CreateScope();
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
